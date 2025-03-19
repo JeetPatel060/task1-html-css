0653267eddaea4b8323d95664a4a236961c74b09
JeetPatel060 <pareljeet1951@gmail.com>
2025-03-19 19:49:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    var navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active'); // Toggle the 'active' class to show/hide links
+});
